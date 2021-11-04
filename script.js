815cddb5de35a72c76603bb6383b291b5f5b74a0
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-11-04 14:01:43 +0100

    file upload + commented js structure

---
+++ script.js
@@ -0,0 +1,19 @@
+// 1 - Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. (nel nostro file js)
+    // Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+
+// 2 - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+
+// 3 - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+
+
+// 1 - Creo un array con all'interno gli oggetti che avranno all'interno dei valori riguardanti il Nome il Ruolo ed il percorso della foto.
+
+// 2 - Stampo tutte le card che abbiamo nell'array nell'html
+    // Prendo dall'html gli id/classi su cui andrò a stampare i valori delle card
+    // Credo un ciclo che dall'array prende l'oggetto e stampa nell'html i valori che ho impostato prima
+
+// 3 - Al click del tasto i valori inseriti nel form andranno a creare un nuovo oggetto da inserire nell'array iniziale
+    // Prendo dall'html gli id associati al form per pescare il testo inserito dall'utente
+    // Al click -
+        // Creo un oggetto che ha i valori inseriti dal form
+        // Pusho l'oggetto creato nell'array iniziale
